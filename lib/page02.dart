3fed1284f88d80fa6fec00994c54c1cab275532b
Rutile3 <Rutile3@outlook.jp>
2020-10-27 20:45:45 +0900

    ✨各Navigatorでの遷移がRouteNameでできることを確認

--- lib/page02.dart
+++ lib/page02.dart
@@ -1,8 +1,43 @@
 import 'package:flutter/material.dart';
 
+import 'page02Routes.dart';
+
 class Page02 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Page02'));
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Page02'),
+          RaisedButton(
+            child: const Text('Push Page02Detail'),
+            onPressed: () {
+              Navigator.pushNamed(context, Page02RouteNames.detail);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Page02Detail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Page02Detail'),
+          RaisedButton(
+            child: const Text('Pop Page02Detail'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/page03.dart
+++ lib/page03.dart
@@ -1,8 +1,43 @@
 import 'package:flutter/material.dart';
 
+import 'page03Routes.dart';
+
 class Page03 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Page03'));
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Page03'),
+          RaisedButton(
+            child: const Text('Push Page03Detail'),
+            onPressed: () {
+              Navigator.pushNamed(context, Page03RouteNames.detail);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Page03Detail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Page03Detail'),
+          RaisedButton(
+            child: const Text('Pop Page03Detail'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
