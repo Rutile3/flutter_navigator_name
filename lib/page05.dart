c6f22f9cbe643760187291f09ce62987e16343a0
ルチルさん <29337073+Rutile3@users.noreply.github.com>
2020-10-28 10:40:26 +0900

    ✨ 各NavigatorでDrawerのページへの遷移ができるよう修正

---
+++ lib/page05.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Page05 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: const Text('Page05'),
+    );
+  }
+}
